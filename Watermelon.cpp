c306f65e5d6ce1c64b17637883bc1a3c68af5c39
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-02-04 14:37:39 +0530

    DIV 2 A

---
+++ Watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define ull unsigned long long 
+#define FIO ios_base::sync_with_stdio(false)
+#define FI cin.tie(NULL)
+#define FO cout.tie(NULL)
+#define endl '\n'
+using namespace std;
+int32_t main(){
+	ll w;
+	cin>>w;
+	if(!(w&1==1)&&(w!=2)) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	
+	
+}
